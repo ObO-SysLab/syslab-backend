78c89bb328539a8da4fde58ad17bf9deae82a7f5
prigigante <asw04015@gmail.com>
2026-04-07 13:34:57 +0900

    feat: 회원가입 기능 구현 (#26)
    * feat: 로그인 기능 구현 (#25)
    
    * fix: JWT 검증 필터 추가 (#25)
    
    * fix: 에러 응답 URL diveon.net으로 수정 (#25)
    
    * [BE] feat: 회원가입 - 일부 구현 업로드,
    구현 내용: requset요청에 맞는 dto 선언
    구현 내용: UserController.java 일부 수정 -  @PostMapoing(/signup) 에 해당하는 컨트롤러의 일부 메서드 중- 입력값과 반환 값 선언만 완료
    구현 내용: Service 객체 신규 생성 - UserSignUpService.java 참고.
    
    * [BE] feat:(#26) 사용자 가입 기능 일부 구현-
    추가 주석메세지 추가한 뒤  커밋임, 이전 커밋을 통해 작업 확인할 것.
    
    * (#26) 회원가입 기능 구현 - 대표 성공사례만 구현
    변경된 파일, UserController AuthSignUpRequest User-entity UserSingUpService SecurityConfig
    변경 사유
    User Controller, 컨트롤러의 껍데기만 구현된 상태에서, 실제 서비스를 호출하고 리턴까지 구현, 다만 TODO를 보면 알겠지만 response에 관해 추가적인 변경이 필요함
    AutSginUpRequest-dto- 문제 생겨서, json키와 변수 매칭시켜주는 어노테이션 전부 삽입 - 차후 변경 필요할지도 모름
    User-entitiy: 레포지토리 삽입을 위한, 새로운 객체 생성이필요하여, 생성자 삽입, 기존 JPA의존성을 위한 기본 생성자도 함께 구현
    UserSignUpService-Service: 비즈니스 로직 구현, 자세한 내용은 해당 주석 참고
    SercurityConfig: Security 의존성에 의해 접근이 차단되는 문제로 인해, 접근허용 endpoint 추가 -다만 추가적인 수정이 필요하질도 모름.
    
    ---------
    
    Co-authored-by: 정민준 <jj353535jj@gmail.com>

--- src/main/java/com/diveon/backend/domain/user/entity/User.java
+++ src/main/java/com/diveon/backend/domain/user/entity/User.java
@@ -56,6 +56,26 @@ public class User {
     @Column(name = "interest")
     private String interest;
 
+    //수정 시작 - 안상완 2026.04.07 - 
+    //유저 정보를 레포지토리에 저장하기 위한 새로운 생성자
+    // 및 기존 JPA를 이용하는 코드를 유지하기 위한 기본 생성자 생성
+    //JPA 의존성을 이용하기 위한, 기본 생성자
+    public User() {};
+    
+    // 회원가입용 생성자
+    //
+    public User(String id, String password, String nickName, String email, String belong, String interest) {
+        this.id = id;
+        this.password = password; // 주의: 실제 서비스에선 암호화 필수!
+        this.nickName = nickName;
+        this.email = email;
+        this.belong = belong;
+        this.interest = interest;
+        this.createdAt = LocalDateTime.now(); // 가입 시간 설정
+        this.tier = 1; // 기본 티어 설정
+    }
+     //수정 종료 - 안상완 2026.04.07 - 
+     
     public String getId() { return id; }
     public String getPassword() { return password; }
     public String getRealName() { return realName; }
