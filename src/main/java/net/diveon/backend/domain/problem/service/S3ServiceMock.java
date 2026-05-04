3fa4612ebf93dc2260725feddcb97ef74d8aca39
sangwan <asw04015@gmail.com>
2026-05-04 23:35:25 +0900

    feat: 코딩형 문제 채점-문제 생성시 테스트케이스 S3 저장 (#145)

--- src/main/java/net/diveon/backend/domain/problem/service/S3ServiceMock.java
+++ src/main/java/net/diveon/backend/domain/problem/service/S3ServiceMock.java
@@ -1,10 +1,13 @@
 package net.diveon.backend.domain.problem.service;
 
+import net.diveon.backend.domain.problem.others.ForDtoTestCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 // 로컬 환경용. 실제 S3 연결 없이 로그만 찍고 끝
 @Service
@@ -16,5 +19,12 @@ public class S3ServiceMock implements S3Service {
     @Override
     public void uploadDockerfileZip(Long probId, MultipartFile file) {
         log.info("[Mock] S3 업로드 생략 - probId: {}, 파일명: {}", probId, file.getOriginalFilename());
+    }
+
+    @Override
+    public void uploadCodingTestcases(Long probId, List<ForDtoTestCase> testcases) {
+        int testcaseCount = testcases != null ? testcases.size() : 0;
+        log.info("[Mock] 코딩형 테스트케이스 S3 업로드 생략 - probId: {}, testcaseCount: {}, prefix: testcases/prob-{}/",
+                probId, testcaseCount, probId);
     }
 }
