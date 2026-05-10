2173b425f26c585d83d9f7313abe752ef8f96950
JeongMinJun <jj353535jj@gmail.com>
2026-05-10 20:18:51 +0900

    feat: 내가 속한 그룹 목록 조회 API 구현 (#168) (#171)

--- src/main/java/net/diveon/backend/domain/group/repository/GroupUserRepository.java
+++ src/main/java/net/diveon/backend/domain/group/repository/GroupUserRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.diveon.backend.domain.group.entity.GroupUser;
 
+import java.util.List;
+
 public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
     long countByGroupId(Long groupId);
     Optional<GroupUser> findByGroupIdAndUserId(Long groupId, Long userId);
+    List<GroupUser> findAllByUserId(Long userId);
 }
