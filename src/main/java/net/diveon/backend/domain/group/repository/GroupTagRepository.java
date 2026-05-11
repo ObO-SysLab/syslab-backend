8b1e7c5a4ea151cf92fa11eb9bc13893beb33981
정민준 <jj353535jj@gmail.com>
2026-05-11 22:32:24 +0900

    fix: 그룹 태그 교체 시 unique 제약 위반 오류 수정 (#194)

--- src/main/java/net/diveon/backend/domain/group/repository/GroupTagRepository.java
+++ src/main/java/net/diveon/backend/domain/group/repository/GroupTagRepository.java
@@ -3,11 +3,17 @@ package net.diveon.backend.domain.group.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import net.diveon.backend.domain.group.entity.GroupTag;
 
 public interface GroupTagRepository extends JpaRepository<GroupTag, Long> {
     List<GroupTag> findAllByGroupId(Long groupId);
     List<GroupTag> findAllByGroupIdIn(List<Long> groupIds);
-    void deleteAllByGroupId(Long groupId);
+
+    @Modifying
+    @Query("DELETE FROM GroupTag gt WHERE gt.group.id = :groupId")
+    void deleteAllByGroupId(@Param("groupId") Long groupId);
 }
